c430f27841799635b28069fc27194c3e0ead26eb
Florent Lejoly <fle@odoo.com>
2019-09-30 13:25:38 +0200

    [IMP] website_[sale_]slides: allow users to take the slide quiz before joining the course
    PURPOSE
    
    When previewing a slide of type 'quiz', users are tempted to answer the
    questions even if they are not member of the course yet.
    
    This commit will allow users to answer the quiz without being a member of the
    course, and offer a smart redirection to join / buy based on the enroll type
    of the channel.
    
    SPECIFICATIONS
    
    We introduce a "CourseJoinWidget" that will handle the display and the behavior
    of the "join" button based on the user (public or not) and the enroll type
    (public course or based on a payment).
    
    When the user answers questions and he's not a member yet, we store the answers
    temporarily in the session object.
    When he comes back on the quiz page after having completed his membership, we
    fill in the quiz based on his previously entered answers and clean the session.
    
    LINKS
    
    PR #41972
    Task 2045571
    
    Co-authored-by: Aurélien Warnon <awa@odoo.com>

---
+++ addons/website_sale_slides/static/src/js/slides_course_join.js
@@ -0,0 +1,45 @@
+odoo.define('website_sale_slides.course.join.widget', function (require) {
+"use strict";
+
+var CourseJoinWidget = require('website_slides.course.join.widget').courseJoinWidget;
+
+CourseJoinWidget.include({
+    xmlDependencies: (CourseJoinWidget.prototype.xmlDependencies || []).concat(
+        ["/website_sale_slides/static/src/xml/slide_course_join.xml"]
+    ),
+    init: function (parent, options) {
+        this._super.apply(this, arguments);
+        this.productId = options.channel.productId || false;
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * When the user joins the course, if it's set as "on payment":
+     * - If the user is logged in, we redirect to the shop page for this course
+     * - If the user is public, we create a login URL that redirects to the shop page afterwards.
+     *
+     * @param {MouseEvent} ev
+     * @override
+     * @private
+     */
+    _onClickJoin: function (ev) {
+        this._super.apply(this, arguments);
+
+        if (this.channel.channelEnroll === 'payment') {
+            var shopUrl = _.str.sprintf('/shop/cart/update?product_id=%s&amp;express=1', this.productId);
+            if (this.publicUser) {
+                shopUrl = _.str.sprintf('/web/login?redirect=%s', encodeURIComponent(shopUrl));
+            }
+            this.beforeJoin().then(function () {
+                window.location.href = shopUrl;
+            });
+        }
+    },
+});
+
+return CourseJoinWidget;
+
+});
