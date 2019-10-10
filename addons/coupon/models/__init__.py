18879db4608f20e104bd6224d08a84835236d9c7
Joseph Caburnay <jcb@odoo.com>
2019-10-10 13:45:35 +0200

    [REF] *_sale_coupon: create coupon module
    This refactoring extracts parts of `sale_coupon` module to create a more
    generic module called `coupon`. This new module can be extended in
    different sales modules to implement functionalities specific to modules
    that extend it.

--- addons/coupon/models/__init__.py
+++ addons/coupon/models/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import sale_coupon
-from . import sale_coupon_reward
-from . import sale_coupon_rules
-from . import sale_coupon_program
-from . import sale_order
+from . import coupon
+from . import coupon_reward
+from . import coupon_rules
+from . import coupon_program
