7f0f11170d9d529cbe20c3f621c27c94297971f5
Julien Castiaux <juc@odoo.com>
2019-10-11 11:55:00 +0200

    [FIX] service: use collections.deque for queue
    In the PreforkServer, the python started process is responsible to
    forward all incoming system signals to the forked processes. Signals
    received are registered in a queue and later consumed.
    
    The queue was implemented on the python list which implements a stack,
    removing the first element from a list is a O(n) operation. The list has
    been replaced by a `collections.deque` (Double Ended QUEue) whose
    operations on either side are O(1).

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -1,6 +1,7 @@
 #-----------------------------------------------------------
 # Threaded, Gevent and Prefork Servers
 #-----------------------------------------------------------
+import collections
 import datetime
 import errno
 import logging
@@ -629,7 +630,7 @@ class PreforkServer(CommonServer):
         self.workers_cron = {}
         self.workers = {}
         self.generation = 0
-        self.queue = []
+        self.queue = collections.deque()
         self.long_polling_pid = None
 
     def pipe_new(self):
@@ -697,8 +698,8 @@ class PreforkServer(CommonServer):
                 self.worker_pop(pid)
 
     def process_signals(self):
-        while len(self.queue):
-            sig = self.queue.pop(0)
+        while self.queue:
+            sig = self.queue.popleft()
             if sig in [signal.SIGINT, signal.SIGTERM]:
                 raise KeyboardInterrupt
             elif sig == signal.SIGHUP:
