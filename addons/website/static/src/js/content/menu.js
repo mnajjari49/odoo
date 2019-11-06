c64d32e10f030403598c39853fc15cfa483acba2
Benjamin Vray <bvr@odoo.com>
2019-11-06 16:01:07 +0100

    [IMP] website: add header visibility options
    Adding a choice of several header behavior in the customize menu (fixed,
    fade out with scroll, etc.)
    
    Part of https://github.com/odoo/odoo/pull/38950
    task-2087641
    
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website/static/src/js/content/menu.js
+++ addons/website/static/src/js/content/menu.js
@@ -4,6 +4,7 @@ odoo.define('website.content.menu', function (require) {
 var dom = require('web.dom');
 var publicWidget = require('web.public.widget');
 var wUtils = require('website.utils');
+var animations = require('website.content.snippets.animation');
 
 publicWidget.registry.affixMenu = publicWidget.Widget.extend({
     selector: 'header.o_affix_enabled',
@@ -142,6 +143,7 @@ publicWidget.registry.autohideMenu = publicWidget.Widget.extend({
                 dom.initAutoMoreMenu(self.$el, {unfoldable: '.divider, .divider ~ li'});
             }
             self.$el.removeClass('o_menu_loading');
+            self.$el.trigger('menu_loaded');
         });
     },
     /**
@@ -231,6 +233,150 @@ publicWidget.registry.menuDirection = publicWidget.Widget.extend({
         }
 
         $menu.addClass('dropdown-menu-' + alignment);
+    },
+});
+
+publicWidget.registry.fixedHeader = publicWidget.Widget.extend({
+    selector: 'header.o_header_fixed',
+
+    /**
+     * @override
+     */
+    start: function () {
+        var menuLoading = this.$('.o_menu_loading');
+        if (!menuLoading.length) {
+            this._initializeFixedHeader();
+        } else {
+            this.$el.one('menu_loaded', () => this._initializeFixedHeader());
+        }
+        return this._super.apply(this, arguments);
+    },
+    /**
+     * @override
+     */
+    destroy: function () {
+        $('main').css('padding-top', '');
+        this.$el.removeClass('o_header_affix affixed');
+        this._super.apply(this, arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    _initializeFixedHeader: function () {
+        $('main').css('padding-top', this.$el.height());
+        this.$el.addClass('o_header_affix affixed');
+    },
+});
+
+const BaseDisappearingHeader = animations.Animation.extend({
+    effects: [{
+        startEvents: 'scroll',
+        update: '_onWindowScroll',
+    }],
+
+    /**
+     * @override
+     */
+    start: function () {
+        this.scrollingDownwards = true;
+        this.hiddenHeader = false;
+        this.position = 0;
+        this.checkPoint = 0;
+        return this._super.apply(this, arguments);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @abstract
+     */
+    _hideHeader: function () {},
+    /**
+     * @private
+     */
+    _showHeader: function () {},
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Called when the window is scrolled
+     *
+     * @private
+     * @param {integer} scrollTop
+     */
+    _onWindowScroll: function (scrollTop) {
+        var scroll = scrollTop;
+        if (scroll > this.position) {
+            if (!this.scrollingDownwards) {
+                this.checkPoint = scroll;
+            }
+            if (!this.hiddenHeader && scroll - this.checkPoint > 200) {
+                this.hiddenHeader = true;
+                this._hideHeader();
+            }
+            this.scrollingDownwards = true;
+        } else {
+            if (this.scrollingDownwards) {
+                this.checkPoint = scroll;
+            }
+            if (this.hiddenHeader && scroll - this.checkPoint < -100) {
+                this.hiddenHeader = false;
+                this._showHeader();
+            }
+            this.scrollingDownwards = false;
+        }
+        this.position = scroll;
+    },
+});
+
+publicWidget.registry.DisappearingHeader = BaseDisappearingHeader.extend({
+    selector: 'header.o_header_disappears',
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _hideHeader: function () {
+        this.$el.animate({top: '-=' + this.$el.height()}, "slow");
+    },
+    /**
+     * @override
+     */
+    _showHeader: function () {
+        this.$el.animate({top: '+=' + this.$el.height()}, "slow");
+    },
+});
+
+publicWidget.registry.FadeOutHeader = BaseDisappearingHeader.extend({
+    selector: 'header.o_header_fade_out',
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _hideHeader: function () {
+        this.$el.fadeOut();
+    },
+    /**
+     * @override
+     */
+    _showHeader: function () {
+        this.$el.fadeIn();
     },
 });
 });
