679032c558978ed8a415b70202aef604ece57c11
Benjamin Frantzen (bfr) <bfr@odoo.com>
2019-12-18 14:45:38 +0100

    Done everything except published button on ratings page

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -402,6 +402,14 @@ class Project(models.Model):
         return action
 
 
+    def action_website_go_to(self):
+        #self.website_id._force() #TODO
+        return {
+            'type': 'ir.actions.act_url',
+            'url': '/',
+            'target': 'self',
+        }
+
     # ---------------------------------------------------
     #  Business Methods
     # ---------------------------------------------------
