d01337421346970e313286ec9ae45d1e9517285c
Lucas Lefèvre <lul@odoo.com>
2019-12-19 09:27:08 +0100

    [REM] calendar: Remove useless is_attendee field
    The field is never used and invisible in the view.

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -190,7 +190,6 @@ class Meeting(models.Model):
 
     name = fields.Char('Meeting Subject', required=True)
 
-    is_attendee = fields.Boolean('Attendee', compute='_compute_attendee')
     attendee_status = fields.Selection(Attendee.STATE_SELECTION, string='Attendee Status', compute='_compute_attendee')
     display_time = fields.Char('Event Time', compute='_compute_display_time')
     start = fields.Datetime('Start', required=True, tracking=True, help="Start date of an event, without time for full days events")
@@ -208,6 +207,7 @@ class Meeting(models.Model):
     show_as = fields.Selection([('free', 'Free'), ('busy', 'Busy')], 'Show Time as', default='busy')
 
     # linked document
+    # LUL TODO use fields.Reference ?
     res_id = fields.Integer('Document ID')
     res_model_id = fields.Many2one('ir.model', 'Document Model', ondelete='cascade')
     res_model = fields.Char('Document Model Name', related='res_model_id.model', readonly=True, store=True)
@@ -266,7 +266,6 @@ class Meeting(models.Model):
     def _compute_attendee(self):
         for meeting in self:
             attendee = meeting._find_my_attendee()
-            meeting.is_attendee = bool(attendee)
             meeting.attendee_status = attendee.state if attendee else 'needsAction'
 
     def _compute_display_time(self):
