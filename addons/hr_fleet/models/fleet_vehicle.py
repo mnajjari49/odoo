0c1b8f5eecf58fceb3ab0b5574930a53d503e9d5
Prakash Prajapati <ppr@odoo.com>
2020-01-09 18:40:05 +0530

    [IMP] hr(_fleet): show license plate on the employee
    Purpose of the task is to find the employee's private vehicle easily.
    
    -License Plate field is added to the Employee Private Address tab.
    -The employee can be searched through the license plate.
    -When the driver of the company's vehicle is assigned, write the
    vehicle number to the employee and set the license plate number
    blank to the employee of previous driver.
    
    Task-ID: 2158962
    Closes: #43491

--- addons/hr_fleet/models/fleet_vehicle.py
+++ addons/hr_fleet/models/fleet_vehicle.py
@@ -13,3 +13,11 @@ class FleetVehicle(models.Model):
     def _compute_mobility_card(self):
         for vehicle in self:
             vehicle.mobility_card = vehicle.driver_id.user_ids[:1].employee_id.mobility_card
+
+    def create_driver_history(self, driver_id):
+        super().create_driver_history(driver_id)
+        driver = self.env['res.partner'].browse(driver_id)
+        for vehicle in self:
+            driver.user_ids.employee_id.write({'license_plate': vehicle.license_plate})
+            if vehicle.driver_id:
+                vehicle.driver_id.user_ids.employee_id.write({'license_plate': False})
