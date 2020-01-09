6a5f81fe870638d05bfa6aaad5a148850aa8224b
Alexandre Kühn <aku@odoo.com>
2020-01-09 15:24:53 +0100

    [FIX] web:tests: remove promise constructor antipattern
    Having async/await as a new promise constructor is an antipattern.
    
    The reason is that it may lead to unnoticed errors: if an inner
    promise is rejected, it won't propagate the error to another promise
    that will handle the error. As a result, this error becomes unnoticed
    and the initial promise is pending indefinitely, which also may lead
    to more bugs [1].
    
    [1] https://stackoverflow.com/a/25569299

--- addons/web/static/tests/helpers/test_utils_dom.js
+++ addons/web/static/tests/helpers/test_utils_dom.js
@@ -20,9 +20,9 @@ var concurrency = require('web.concurrency');
  *
  * @returns {Promise}
  */
-function returnAfterNextAnimationFrame() {
-    return new Promise(async resolve => {
-        await concurrency.delay(0);
+async function returnAfterNextAnimationFrame() {
+    await concurrency.delay(0);
+    return new Promise(resolve => {
         window.requestAnimationFrame(resolve);
     });
 }
