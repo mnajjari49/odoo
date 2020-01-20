95168d60b28ac2399aac65728f3935adcfb6d08d
Lucas Perais (lpe) <lpe@odoo.com>
2020-01-20 17:43:05 +0100

    [FIX] fieldregistrowl refactor
    TODO import compatibility

--- addons/web/static/src/js/fields/field_registry_owl.js
+++ addons/web/static/src/js/fields/field_registry_owl.js
@@ -4,37 +4,50 @@ odoo.define('web.field_registry_owl', function (require) {
 var field_registry = require('web.field_registry');
 var Class = require('web.Class');
 var AbstractField = require('web.AbstractField');
+const WidgetAdapterMixin;
+const ComponentWrapper;
 
-var FieldRegistryOwl = Class.extend({
-    /**
-     *  Act as a proxy to add an Owl widget into the field registry
-     */
-    add: function(name, renderer) {
-
-        var OwlAbstractWidget = AbstractField.extend({
-            start: function () {
-                this.renderer = new renderer();
-                return this._super.apply(this, arguments);
-            },
-            reset: function(record, event) {
-                this.renderer.updateProps(this.value);
-                return this._super.apply(this, arguments);
-            },
-            _render: function() {
-                let prom;
-                this.renderer.updateProps(this.value);
-                if (this.renderer.__owl__.isMounted) {
-                    prom = this.renderer.render();
-                } else {
-                    prom = this.renderer.mount(this.$el[0], true);
-                }
-                return prom;
-            }
-        });
-        field_registry.add(name, OwlAbstractWidget);
+var FieldAdapter = AbstractField.extend(WidgetAdapterMixin ,{
+    start: function () {
+        this.component = new this.ComponentWrapper(this, this.ComponentClass, this.value);
+        return this._super.apply(this, arguments);
     },
+    reset: function(record, event) {
+        this.component.updateProps(this.value);
+        return this._super.apply(this, arguments);
+    },
+    _render: function() {
+        let prom;
+        this.component.updateProps(this.value);
+        if (this.component.__owl__.isMounted) {
+            prom = this.component.render();
+        } else {
+            prom = this.component.mount(this.$el[0], true);
+        }
+        return prom;
+    }
 });
 
-return new FieldRegistryOwl();
+function addToFieldRegistry(key, value, score) {
+    const fieldClass = value;
+    if (fieldClass.prototype instanceof Component) { // to check
+        value = FieldAdapter.extend({
+            ComponentClass: fieldClass,
+        });
+    field_registry.add(key, value);
+    }
+}
+
+return {
+    FieldAdapter,
+    addToFieldRegistry,
+}
+
+// const registry_add = field_registry.add;
+// field_registry.add = function (key, value, score) {
+//     const fieldClass = value;
+    
+//     registry_add.call(field_registry, key, value, score);
+// }
 
 });
