yf956613/odoo-1

[FIX] sale_mrp: change the test case accordingly UOM precision change
purpose of this commit is in master two time rounding is done
first base on uom rounding and second is base on decimal accuracy
of UoM digits that is given in field level...
In our case we are doing a single rounding i.e base on uom
rounding so here we need to change the test case

Ex: In master it will do two times converstion first
with uom rounding(with rounding method 'up') and
second with digits(without rounding method)

value=0.5833333333333333
precision_rounding=0.000001
rounding_method='UP'
=================
result=0.583334
================
After that it will doing fields level rounding by digits
which may different then uom rounding.

value = 0.583334
precision_digits=3
===============
result= 0.583

In our case it will doing single rounding

value=0.5833333333333333
precision_rounding=0.001
rounding_method='UP'
============
result=0.584
============

task-1914025

addons/sale_mrp/tests/test_sale_mrp_flow.py
@@ -1409,6 +1409,7 @@ class TestSaleMrpFlow(common.SavepointCase):
         # 2x Dozens kit_1 --|- component_unit   x6 Units
         #                   |- component_kg     x7 Kg
 
+        self.uom_kg.rounding = 0.001
         kit_1 = self._create_product('Kit1', self.uom_unit)
         component_unit = self._create_product('Comp Unit', self.uom_unit)
         component_kg = self._create_product('Comp Kg', self.uom_kg)
@@ -1454,6 +1455,7 @@ class TestSaleMrpFlow(common.SavepointCase):
 
         # Finally, we check the quantities for each component on the picking
         move_component_unit = order.picking_ids[0].move_lines.filtered(lambda m: m.product_id == component_unit)
+
         move_component_kg = order.picking_ids[0].move_lines - move_component_unit
         self.assertEquals(move_component_unit.product_uom_qty, 0.5)
-        self.assertEquals(move_component_kg.product_uom_qty, 0.583)
+        self.assertEquals(move_component_kg.product_uom_qty, 0.584)

mnajjari49/odoo

addons/sale_mrp/tests/test_sale_mrp_flow.py
@@ -1446,6 +1446,7 @@ class TestSaleMrpFlow(AccountTestCommon):
         # 2x Dozens kit_1 --|- component_unit   x6 Units
         #                   |- component_kg     x7 Kg
 
+        self.uom_kg.rounding = 0.001
         kit_1 = self._create_product('Kit1', self.uom_unit)
         component_unit = self._create_product('Comp Unit', self.uom_unit)
         component_kg = self._create_product('Comp Kg', self.uom_kg)
@@ -1491,9 +1492,10 @@ class TestSaleMrpFlow(AccountTestCommon):
 
         # Finally, we check the quantities for each component on the picking
         move_component_unit = order.picking_ids[0].move_lines.filtered(lambda m: m.product_id == component_unit)
+
         move_component_kg = order.picking_ids[0].move_lines - move_component_unit
         self.assertEqual(move_component_unit.product_uom_qty, 0.5)
-        self.assertEqual(move_component_kg.product_uom_qty, 0.583)
+        self.assertEqual(move_component_kg.product_uom_qty, 0.584)
 
     def test_product_type_service_1(self):
         route_manufacture = self.warehouse.manufacture_pull_id.route_id.id